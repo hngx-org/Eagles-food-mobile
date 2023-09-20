1d89b39efef878403eb23aae8648895bab619555
QuadlcorE <62242772+QuadlcorE@users.noreply.github.com>
2023-09-20 08:29:41 +0100

    Fixed the spacing of the buttons.

--- lib/screens/send_lunch/send_lunch_success.dart
+++ lib/screens/send_lunch/send_lunch_success.dart
@@ -7,58 +7,63 @@ class SendLunchSuccess extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: SingleChildScrollView(
-        child: Center(
-          // TODO: Spread out the widgets
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Image.asset("assets/images/send-lunch-success.png"),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  const Text(
-                    "Your Free Lunch has been sent successfully",
-                    style: TextStyle(
-                      fontFamily: 'Poppins',
-                      fontSize: 24,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 28, vertical: 12),
-                    // TODO: Fix the spacing of the text widget.
-                    child: Text(
-                      "Congratulations, you have successfully sent brooklyn Simmons 3 Free Lunches! Click continue to proceed",
-                      textAlign: TextAlign.center,
+        // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Center(child: Image.asset("assets/images/send-lunch-success.png")),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                const Column(
+                  children: [
+                    Text(
+                      "Your Free Lunch has been sent successfully",
                       style: TextStyle(
-                        fontSize: 14,
-                        fontWeight: FontWeight.w400,
+                        fontFamily: 'Poppins',
+                        fontSize: 24,
+                        fontWeight: FontWeight.w600,
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 29, vertical: 60),
-                    child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            minimumSize: const Size.fromHeight(50),
-                            backgroundColor: const Color(0xFF04754D)),
-                        onPressed: () {},
-                        child: const Text(
-                          "CONTINUE",
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        )),
-                  ),
-                ],
-              ),
-            ],
+                    Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 28, vertical: 12),
+                      // TODO: Fix the spacing of the text widget.
+                      child: Text(
+                        "Congratulations, you have successfully sent brooklyn Simmons 3 Free Lunches! Click continue to proceed",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+
+                // Continue Button
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 29, vertical: 60),
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(),
+                          minimumSize: const Size.fromHeight(50),
+                          backgroundColor: const Color(0xFF04754D)),
+                      onPressed: () {},
+                      child: const Text(
+                        "CONTINUE",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      )),
+                ),
+              ],
+            ),
           ),
-        ),
+        ],
       )),
     );
   }
