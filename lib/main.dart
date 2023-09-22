9bd8242afa4561ee6bb92743fb0b83b99cf372ae
OGZod <obenayuk443@gmail.com>
2023-09-23 00:43:08 +0100

    change_logo

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:hng_task3/configs/theme.dart';
 import 'package:hng_task3/providers/AuthProvider.dart';
 import 'package:hng_task3/screens/menu/components/nav_screen.dart';
-import 'package:hng_task3/screens/splashscreen/splashscreen.dart';
-// import 'package:hng_task3/screens/splashscreen/splashscreen.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -23,7 +21,8 @@ class MainApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         theme: theme,
         darkTheme: theme,
-        home: const SplashScreen(),
+        home: const NavScreen()
+        //SplashScreen(),
       ),
     );
   }
