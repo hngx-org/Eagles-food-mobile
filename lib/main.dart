892cac17dd6ec67e44fc334325565920d757f28e
Phosah <efosa.uyi@gmail.com>
2023-09-23 21:35:47 +0100

    General cleanup of codebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hng_task3/configs/theme.dart';
 import 'package:hng_task3/providers/AuthProvider.dart';
 import 'package:hng_task3/providers/num_of_free_lunch_provider.dart';
+import 'package:hng_task3/screens/menu/components/nav_screen.dart';
 import 'package:hng_task3/screens/splashscreen/splashscreen.dart';
 import 'package:provider/provider.dart';
 
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -19,10 +19,11 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   List<String> employees = [
-    'John Doe',
-    'Jane Smith',
-    'Mark Johnson',
-    'Emily Davis',
+    'Oben Ayuk Gilbert Abunaw',
+    'Efosa Uyi-Idahor',
+    'akamsr',
+    'Godwin Adah',
+    'Aaron Ogbemi',
   ];
 
   String selectedEmployee = '';
@@ -72,9 +73,7 @@ class _HomeScreenState extends State<HomeScreen> {
                       children: [
                         TextSpan(
                             text: 'Good ',
-                            style: Theme.of(context).textTheme.bodyLarge
-                            //?.copyWith(fontWeight: FontWeight.w700),
-                            ),
+                            style: Theme.of(context).textTheme.bodyLarge),
                         TextSpan(
                           text: Utils.getTimeOfDay(),
                           style: Theme.of(context).textTheme.bodyLarge?.copyWith(
@@ -103,7 +102,8 @@ class _HomeScreenState extends State<HomeScreen> {
           const SizedBox(
             height: 30,
           ),
-          searchEmployeeBox(employees, (p0) => null, selectedEmployee, focusNode),
+          searchEmployeeBox(
+              employees, (p0) => null, selectedEmployee, focusNode),
           const SizedBox(
             height: 18,
           ),
