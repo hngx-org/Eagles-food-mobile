85be8817c8ff336a7a3786d05ec5a159881cbacb
Moyinoluwa Shabi <shabimoyinoluwa19@gmail.com>
2023-10-19 04:21:43 +0100

    add search functionality [100% working now]
    - successfully integrated the 2 search apis from the backend

--- lib/components/widgets/send_lunch/my_team_search.dart
+++ lib/components/widgets/send_lunch/my_team_search.dart
@@ -20,101 +20,90 @@ class _MyTeamSearchState extends State<MyTeamSearch> {
 
   @override
   Widget build(BuildContext context) {
-    if (widget.list.length > 0) {
-      return widget.list.isEmpty
-          ? ListView.builder(
-              shrinkWrap: true,
-              padding: const EdgeInsets.symmetric(vertical: 0),
-              physics: const NeverScrollableScrollPhysics(),
-              itemCount: 5,
-              itemBuilder: (context, index) => const TeamShimmer())
-          : NotificationListener<ScrollEndNotification>(
-              onNotification: (scrollEnd) {
-                var metrics = scrollEnd.metrics;
-                if (metrics.atEdge) {
-                  if (metrics.pixels == 0) {
-                  } else {
-                    setState(() {
-                      end_reached = true;
-                      page++;
-                    });
-                    Provider.of<TeamAndLunchProvider>(context, listen: false)
-                        .getMyTeam(page, '');
-                  }
+    return widget.list.isEmpty
+        ? ListView.builder(
+            shrinkWrap: true,
+            padding: const EdgeInsets.symmetric(vertical: 0),
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: 5,
+            itemBuilder: (context, index) => const TeamShimmer())
+        : NotificationListener<ScrollEndNotification>(
+            onNotification: (scrollEnd) {
+              var metrics = scrollEnd.metrics;
+              if (metrics.atEdge) {
+                if (metrics.pixels == 0) {
+                } else {
+                  setState(() {
+                    end_reached = true;
+                    page++;
+                  });
+                  Provider.of<TeamAndLunchProvider>(context, listen: false)
+                      .getMyTeam(page, '');
                 }
-                return true;
-              },
-              child: Expanded(
-                child: ListView.builder(
-                  itemCount: widget.list.length,
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                  itemBuilder: (context, index) {
-                    final item = widget.list[index];
-                    return SizedBox(
-                        width: double.infinity,
-                        child: ListTile(
-                          contentPadding: const EdgeInsets.all(0),
-                          leading: CircleAvatar(
-                            backgroundImage: AssetImage(item.image),
-                          ),
-                          title: Text(
-                            item.name,
-                            style: Theme.of(context)
-                                .textTheme
-                                .displayMedium
-                                ?.copyWith(
-                                    fontSize: 16, fontWeight: FontWeight.w400),
-                          ),
-                          subtitle: Text(
-                            '${item.email}',
-                            style: Theme.of(context)
-                                .textTheme
-                                .headlineSmall
-                                ?.copyWith(
-                                    fontWeight: FontWeight.w500, fontSize: 11),
-                          ),
-                          trailing: Container(
-                            padding: const EdgeInsets.symmetric(horizontal: 4),
-                            color:
-                                Theme.of(context).brightness == Brightness.dark
-                                    ? ColorUtils.Green
-                                    : ColorUtils.Yellow,
-                            child: TextButton(
-                              onPressed: () {
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => SendLunchScreen(
-                                              receiver: item,
-                                            )));
-                              },
-                              child: Text(
-                                'Send Lunch',
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .bodyLarge
-                                    ?.copyWith(
-                                        fontWeight: FontWeight.w700,
-                                        fontSize: 13),
-                              ),
+              }
+              return true;
+            },
+            child: Expanded(
+              child: ListView.builder(
+                itemCount: widget.list.length,
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                itemBuilder: (context, index) {
+                  final item = widget.list[index];
+                  return SizedBox(
+                      width: double.infinity,
+                      child: ListTile(
+                        contentPadding: const EdgeInsets.all(0),
+                        leading: CircleAvatar(
+                          backgroundImage: AssetImage(item.image),
+                        ),
+                        title: Text(
+                          item.name,
+                          style: Theme.of(context)
+                              .textTheme
+                              .displayMedium
+                              ?.copyWith(
+                                  fontSize: 16, fontWeight: FontWeight.w400),
+                        ),
+                        subtitle: Text(
+                          '${item.email}',
+                          style: Theme.of(context)
+                              .textTheme
+                              .headlineSmall
+                              ?.copyWith(
+                                  fontWeight: FontWeight.w500, fontSize: 11),
+                        ),
+                        trailing: Container(
+                          padding: const EdgeInsets.symmetric(horizontal: 4),
+                          color: Theme.of(context).brightness == Brightness.dark
+                              ? ColorUtils.Green
+                              : ColorUtils.Yellow,
+                          child: TextButton(
+                            onPressed: () {
+                              FocusManager.instance.primaryFocus
+                                  ?.unfocus(); // dismiss keyboard if open
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => SendLunchScreen(
+                                            receiver: item,
+                                          )));
+                            },
+                            child: Text(
+                              'Send Lunch',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodyLarge
+                                  ?.copyWith(
+                                      fontWeight: FontWeight.w700,
+                                      fontSize: 13),
                             ),
                           ),
-                        ));
-                  },
-                ),
+                        ),
+                      ));
+                },
               ),
-            );
-    } else {
-      return ListTile(
-        title: Text(
-          "No user found",
-          style: Theme.of(context)
-              .textTheme
-              .displayMedium
-              ?.copyWith(fontSize: 16, fontWeight: FontWeight.w400),
-        ),
-      );
-    }
+            ),
+          );
   }
 }
